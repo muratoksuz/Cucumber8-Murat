385f1d864cde640e7f1dc3c8992af8fbbc6c26bf
muratoksuz <murattoksuz@gmail.com>
2025-05-29 20:46:55 +0300

    all in

--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -4,9 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "@SmokeTest and @Regression",
+        tags = "@Regression",
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinitions"}
+        ,plugin= {"json:target/cucumber/cucumber.json"}
 )
 
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
