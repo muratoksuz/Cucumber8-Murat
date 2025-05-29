091675341b134b412f91fdbea3b90f6bccf3fc64
muratoksuz <murattoksuz@gmail.com>
2025-05-29 20:28:09 +0300

    all in

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -2,6 +2,7 @@ package Utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -27,13 +28,31 @@ public class GWD {
             switch (threadBrowserName.get()){
                 case "edge" : threadDriver.set(new EdgeDriver());
                 case "firefox" : threadDriver.set(new FirefoxDriver());
-                default: threadDriver.set(new ChromeDriver());
+                default:
+
+                    if (isRunningOnJenkins()) {
+                        // madem jenkins de çalışıyorsun max boyutta ve başlıksız çalış
+                        //Jenkins için Chrome memory maximize edildi ve hafızada çalışır hale getirildi
+                        ChromeOptions ChromeOptions = new ChromeOptions();
+                        ChromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
+
+                        threadDriver.set(new ChromeDriver(ChromeOptions));
+                    }
+                    else
+                    {
+                        threadDriver.set(new ChromeDriver());// bu thread e bir chrome oluştur ve set et
+                    }
             }
 
             threadDriver.get().manage().window().maximize();
             threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         }
         return threadDriver.get();
+    }
+
+    public static boolean isRunningOnJenkins() {
+        String jenkinsHome = System.getenv("JENKINS_HOME");
+        return jenkinsHome != null && !jenkinsHome.isEmpty();
     }
 
     public static void quitDriver() {
